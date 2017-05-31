53f483dac0e01eb8037331498ae0688e69fd0008
tcrurav <tiburcio.cruz@gmail.com>
2017-05-31 12:34:54 +0100

    modificado el manifest

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
+    'author': "Tiburcio S.L.",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
